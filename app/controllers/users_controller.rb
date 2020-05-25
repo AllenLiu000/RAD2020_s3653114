1593e9d807a7137c19fdca3d423dc768fd1ab3ce
AllenLiu000 <s3653114@student.rmit.edu.au>
2020-05-25 23:54:06 +1000

    AFix footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      Active.create(user_id: @user.id)
+      flash[:success] = "Welcome to the News+!"
       redirect_to @user
     else
       render 'new'
